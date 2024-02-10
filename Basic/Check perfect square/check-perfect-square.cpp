075cc99996b3572ed3f474847e502d8587851110
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2024-02-10 22:27:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check perfect square/check-perfect-square.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int checkPerfectSquare(int N){
+        if(N == 1) return true;
+        for(long long i  = 1; i<=N/3;i++){
+            if(i*i == N){
+                return true;
+            }
+        } 
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.checkPerfectSquare(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
